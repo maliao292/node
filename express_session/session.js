b3cbecedf99d6bdde49ffcb501d6adef75cd8cbe
maliao292 <maming292@163.com>
2019-09-29 17:41:39 +0800

    Mongodb

--- express_session/session.js
+++ express_session/session.js
@@ -1,29 +1,56 @@
 let express = require('express');
 let app = new express();
 let session = require('express-session');
-session.use(session({
-    secret:'mario',  //生成加密字符串的‘秘钥’
-    resave:false, // 强制保存
-    saveUninitialized:true, // 即使他并没有什么变化
-    cookie:{
-        maxAge:1000*60*30, // 过期时间
+
+/* 连接服务器模块 */
+let MongoStore = require('connect-mongo')(session);
+app.use(session({
+    secret: 'mario',  //生成加密字符串的‘秘钥’
+    resave: false, // 强制保存
+    saveUninitialized: true, // 即使他并没有什么变化
+    cookie: {
+        maxAge: 1000 * 60 * 30, // 过期时间
     },
-    rolling:true // 在每次请求时强行设置cookie 将重置cookie 过期时间（默认 false）
+    store: new MongoStore({
+        url: 'mongodb://127.0.0.1:27017/mario',
+        touchAction: 60 * 1000 //
+    }),
+    rolling: true // 在每次请求时强行设置cookie 将重置cookie 过期时间（默认 false）
 }));
-app.set('/set',function(req,res){
+app.get('/set', function (req, res) {
+    req.session.userInfo = '李四';
+    res.send('登录成功')
+});
+
+app.get('/dologin', function (req, res) {
     req.session.userInfo = '张三';
+    res.send('登录成功')
 });
-app.get('/login',function (req,res) {
-   if(req.session.userInfo){
-       res.send('你好，'+req.session.userInfo+'欢迎回来')
-   } else {
-       res.send('未登录');
-       window.location.href = '127.0.0.1:8888/set'
-   }
+app.get('/home', function (req, res) {
+    if (req.session.userInfo) {
+        res.send('你好，' + req.session.userInfo + '欢迎回来')
+    } else {
+        res.send('未登录');
+    }
 });
-app.get('/',function (req,res) {
-    res.send('res');
+
+// 退出登录 销毁session
+app.get('/logOut', function (req, res) {
+    req.session.destroy(function (err) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send('已经退出')
+        }
+    })
+});
+app.get('/', function (req, res) {
+    if (req.session.userInfo) {
+        res.send('你好，' + req.session.userInfo + '欢迎回来')
+    } else {
+        res.send('未登录');
+    }
 });
 
 
-app.listen(8888,'127.0.0.1');
+app.listen(8888, '127.0.0.1');
---
+++ z_pro/app.js
@@ -0,0 +1,94 @@
+let express = require('express');
+let app = new express();
+app.set('view engine', 'ejs');
+app.use(express.static('static'));
+app.get('/', function (req, res) {
+    let msg = '你好';
+    res.send('index');
+});
+
+/*body-parser*/
+let bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+/* 连接数据库 */
+let MongoClient = require('mongodb').MongoClient;
+let Dburl = 'mongodb://127.0.0.1:27017/z_pro';
+
+app.use(function (req,res,next) {
+    next();
+});
+
+
+// 登录
+app.get('/login', function (req, res) {
+    let msg = '你好吗';
+    res.render('login', {msg});
+});
+app.post('/dologin', function (req, res) {
+
+    MongoClient.connect(Dburl, function (err, db) {
+        if (err) {
+            console.log('连接数据失败', err)
+        } else {
+// 方法二
+            let msg = req.body;
+            console.log(msg);
+            let {username,password} = msg;
+            let result = db.collection('user').find({username,password});
+            result.toArray(function (err,data) {  // 遍历数据的方法
+                console.log(data);
+                db.close();
+                if(data.length === 0){
+                 //   res.send({msg:1})
+                   res.send('<script>location.href="/login";alert("登陆失败");</script>')
+                }else{
+                    res.redirect('/product')
+                }
+            })
+
+            /*   方法一、
+                let list = [];
+                   let msg = req.body;
+                   let {username,password} = msg;
+                   let result = db.collection('user').find({username});
+                   result.each(function (error,doc) {
+                       if(error){
+                           console.log('查询user数据失败',err)
+                       }else{
+                           if(doc != null){
+                               list.push(doc)
+                           }else {
+                               let isNext = '0';
+                               if(list[0].password === password){
+                                   isNext = '1';
+                               }else{
+                                   isNext = '0';
+                               }
+                               res.send(isNext);
+                           }
+                       }
+                   });*/
+
+        }
+    })
+
+});
+// 商品
+app.get('/product', function (req, res) {
+    res.render('product');
+});
+// 商品 新增
+app.get('/productadd', function (req, res) {
+    res.render('productadd');
+});
+// 商品 编辑
+app.get('/productedit', function (req, res) {
+    res.render('productedit');
+});
+// 商品 删除
+app.get('/productdelete', function (req, res) {
+    res.send('productdelete');
+});
+app.listen(8000, '127.0.0.1');
