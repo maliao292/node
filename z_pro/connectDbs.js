44c56fd7e86588f255aafbff2fec0a734dc0f9a1
maliao292 <maming292@163.com>
2019-11-09 18:42:57 +0800

    反向代理

--- z_pro/connectDbs.js
+++ z_pro/connectDbs.js
@@ -3,9 +3,7 @@ let app = new express();
 let MongoClient = require('mongodb').MongoClient;
 let Dburl = 'mongodb://127.0.0.1:27017/z_pro';
 app.get('/api/list', function (req, res) {
-    MongoClient.connect(Dburl, function (err, db) {
-
-
+    MongoClient.connect(Dburl, function (err, db)
         var list = [];
         var dataarr = db.collection('user').find()
         dataarr.each(function (err, doc) {
